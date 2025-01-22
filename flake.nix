3047d45147a341c5785d2dd7c490203081735858
Xerz <xerz@xerz.one>
2025-01-22 20:12:14 +0000

    Added basic feature parity with upstream flake

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     sysrepo.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     agenix = {
       url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "sysrepo";	
+      inputs.nixpkgs.follows = "sysrepo";
     };
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
@@ -14,18 +14,37 @@
   };
 
   outputs =
-    { self, sysrepo, agenix, ... }@inputs:
+    {
+      self,
+      sysrepo,
+      agenix,
+      ...
+    }@inputs:
     let
       forAllSystems = with sysrepo.lib; genAttrs (import agenix.inputs.systems);
     in
     {
+      nixosModules = agenix.nixosModules;
+      darwinModules = agenix.darwinModules;
+      homeManagerModules = agenix.homeManagerModules;
+
+      overlays.default =
+        final: prev:
+        let
+          pkgs = prev.extend inputs.lix-module.overlays.default;
+        in
+        {
+          agenix = prev.callPackage "${inputs.agenix}/pkgs/agenix.nix" { };
+        };
+
       packages = forAllSystems (
         system:
         let
           pkgs = sysrepo.legacyPackages.${system}.extend inputs.lix-module.overlays.default;
         in
         rec {
-          agenix = pkgs.callPackage "${inputs.agenix}/pkgs/agenix.nix" {};
+          agenix = pkgs.callPackage "${inputs.agenix}/pkgs/agenix.nix" { };
+          doc = pkgs.callPackage "${inputs.agenix}/pkgs/doc.nix" { inherit self; };
           default = agenix;
         }
       );
