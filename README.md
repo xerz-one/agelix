ebeb7e221998bf120d4d231b45ae1fc64d2f0981
Xerz <xerz@xerz.one>
2025-01-22 18:48:29 +0000

    Initial commit (based on `agenix` commit `e600439`)

---
+++ README.md
@@ -0,0 +1,9 @@
+# `agelix`
+
+This is [`agenix`](https://github.com/ryantm/agenix) but with Lix.
+
+Yep.
+
+Just, pretty much that.
+
+I did this because I'm a heavy user of pipe operators and `agenix` quickly complained about that. It relies on the official Lix overlay, as `agenix` looks for the `nix` package on Nixpkgs ‒ thus providing a readily available derivation as a flake that makes life a bit easier!
---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Agenix with Lix";
+
+  inputs = {
+    sysrepo.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "sysrepo";	
+    };
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+      inputs.nixpkgs.follows = "sysrepo";
+    };
+  };
+
+  outputs =
+    { self, sysrepo, agenix, ... }@inputs:
+    let
+      forAllSystems = with sysrepo.lib; genAttrs (import agenix.inputs.systems);
+    in
+    {
+      inherit inputs;
+      
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = sysrepo.legacyPackages.${system}.extend inputs.lix-module.overlays.default;
+        in
+        rec {
+          agenix = pkgs.callPackage "${inputs.agenix}/pkgs/agenix.nix" {};
+          default = agenix;
+        }
+      );
+    };
+}
